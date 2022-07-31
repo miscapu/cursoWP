48adb2b6d040088b1ff8a03cff6cc6405546e369
miscapu <mignan2010@outlook.com>
2022-07-31 19:54:28 -0300

    Add function get_header and get_footer for call header and footer php file

--- wp-content/themes/miscapu/index.php
+++ wp-content/themes/miscapu/index.php
@@ -1,42 +1,5 @@
-<!doctype html>
-<!-- Add attributes language -->
-<html <?php language_attributes();?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' );?>">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Remove title -->
-<!--    <title>Course WP</title>-->
-</head>
-<body <?php body_class();?>>
-
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-		when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		It has survived not only five centuries, but also the leap into electronic typesetting,
-		remaining essentially unchanged.
-	</p>
-
-    <div id="page" class="site">
-        <!-- Header -->
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    Logo
-                </div>
-                <div class="searchbox">
-                    Form Search
-                </div>
-            </section>
-            <section class="menu-area">
-                <nav class="main-menu">
-                    Menu Nav
-                </nav>
-            </section>
-        </header>
-        <!-- End Header -->
-
+<!-- Call header.php file -->
+<?php get_header(); ?>
         <!-- Content -->
         <div id="content" class="site-content">
             <div class="primary" id="content-area">
@@ -54,16 +17,5 @@
             </div>
         </div>
         <!-- End Content -->
-
-        <!-- Add footer -->
-            <footer class="site-footer">
-                Footer
-            </footer>
-        <!-- End Footer -->
-
-    </div>
-
-
-
-</body>
-</html>
+<!-- Call footer.php file -->
+<?php get_footer(); ?>
