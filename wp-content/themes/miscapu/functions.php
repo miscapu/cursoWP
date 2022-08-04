edb742ba2934c78cc8b4fbb535351c72053416fc
miscapu <mignan2010@outlook.com>
2022-08-03 22:30:54 -0300

    Add dropdown.js file in footer with wp_enqueue_script function

--- wp-content/themes/miscapu/functions.php
+++ wp-content/themes/miscapu/functions.php
@@ -7,5 +7,9 @@ function miscapu_load_scripts()
 		get_template_directory() .'/style.css' ), 'all' );
 	// Add Google fonts
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
+
+	// Add JS
+    // get_template_directory_uri: identificador da página que leva até a página do tema
+    wp_enqueue_script( 'dropdown', get_template_directory_uri().'/js/dropdown.js', array(), '1.0', true  );
 }
 add_action( 'wp_enqueue_scripts', 'miscapu_load_scripts' );
