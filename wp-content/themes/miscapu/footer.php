0a5ed80182d39d0aee0309db5691d6739730348e
miscapu <mignan2010@outlook.com>
2022-08-06 08:11:02 -0300

    Add menu in footer file using wp_nav_menu functions and array theme_location slug functions.php and depth for menu nivel

--- wp-content/themes/miscapu/footer.php
+++ wp-content/themes/miscapu/footer.php
@@ -1,6 +1,18 @@
 <!-- Add footer -->
 <footer class="site-footer">
-	Footer
+	<div class="container">
+        <div class="copyright">
+            <p>Copyright X - All Rights Reserved </p>
+        </div>
+        <div class="footer-menu">
+            <?php wp_nav_menu(
+                    array(
+                            'theme_location'    =>  'miscapu_footer_menu',
+                            'depth'             =>  1
+                    )
+            );?>
+        </div>
+    </div>
 </footer>
 <!-- End Footer -->
 
