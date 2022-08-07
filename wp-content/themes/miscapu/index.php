79565352b761fb5a4296d105f6012920d6dcfd69
miscapu <mignan2010@outlook.com>
2022-08-07 09:20:53 -0300

    Home template of our theme, configured into configurations/read/staticpage -> home and blog page blog

--- wp-content/themes/miscapu/index.php
+++ wp-content/themes/miscapu/index.php
@@ -1,21 +1,31 @@
-<!-- Call header.php file -->
 <?php get_header(); ?>
-        <!-- Content -->
-        <div id="content" class="site-content">
-            <div class="primary" id="content-area">
-                <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
-                    </section>
-                    <section class="services">
-                        Services
-                    </section>
-                    <section class="home-blog">
-                        Blog
-                    </section>
-                </main>
-            </div>
+    <div id="content" class="site-content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <h1>Blog</h1>
+                <div class="container">
+                    <div class="blog-items">
+			            <?php
+			            if( have_posts() ):
+				            while( have_posts() ) : the_post();
+					            ?>
+                                <article>
+                                    <h2><?php the_title(); ?></h2>
+                                    <div class="meta-info">
+                                        <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                        <p>Categories: <?php the_category( ' ' ); ?></p>
+                                        <p>Tags: <?php the_tags( '', ', '); ?></p>
+                                    </div>
+						            <?php the_content(); ?>
+                                </article>
+				            <?php
+				            endwhile;
+			            else: ?>
+                            <p>Nothing yet to be displayed!</p>
+			            <?php endif; ?>
+                    </div>
+                </div>
+            </main>
         </div>
-        <!-- End Content -->
-<!-- Call footer.php file -->
+    </div>
 <?php get_footer(); ?>
