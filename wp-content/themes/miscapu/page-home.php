027d6207f472b3fe8957c1bc074dfb78d4ee94ea
miscapu <mignan2010@outlook.com>
2022-08-20 07:13:24 -0300

    Create Sidebar Services: using conditional is_active_sidebar, function dynamic_sidebar and register sidebar into functions.php file and create sidebar into widgets admin wp

--- wp-content/themes/miscapu/page-home.php
+++ wp-content/themes/miscapu/page-home.php
@@ -2,29 +2,60 @@
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
-				<h1>Page-home</h1>
-				<div class="container">
-					<div class="blog-items">
-						<?php
-						if( have_posts() ):
-							while( have_posts() ) : the_post();
-								?>
-								<article>
-									<h2><?php the_title(); ?></h2>
-									<div class="meta-info">
-										<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-										<p>Categories: <?php the_category( ' ' ); ?></p>
-										<p>Tags: <?php the_tags( '', ', '); ?></p>
-									</div>
-									<?php the_content(); ?>
-								</article>
+                <section class="hero">
+                    Hero
+                </section>
+                <section class="services">
+                    <h2>Services</h2>
+                    <div class="container">
+                        <div class="services-item">
+                            <?php
+                                if ( is_active_sidebar( 'services-1' ) ){
+	                                dynamic_sidebar( 'services-1' );
+                                }
+                            ?>
+                        </div>
+                        <div class="services-item">
+                            <?php
+                                if ( is_active_sidebar( 'services-2' ) ){
+	                                dynamic_sidebar( 'services-2' );
+                                }
+
+                            ?>
+                        </div>
+                        <div class="services-item">
+                            <?php
+                                if ( is_active_sidebar( 'services-3' ) ){
+	                                dynamic_sidebar( 'services-3' );
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </section>
+				<section class="home-blog">
+                    <div class="container">
+                        <div class="blog-items">
 							<?php
-							endwhile;
-						else: ?>
-							<p>Nothing yet to be displayed!</p>
-						<?php endif; ?>
-					</div>
-				</div>
+							if( have_posts() ):
+								while( have_posts() ) : the_post();
+									?>
+                                    <article>
+                                        <h2><?php the_title(); ?></h2>
+                                        <div class="meta-info">
+                                            <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                            <p>Categories: <?php the_category( ' ' ); ?></p>
+                                            <p>Tags: <?php the_tags( '', ', '); ?></p>
+                                        </div>
+										<?php the_content(); ?>
+                                    </article>
+								<?php
+								endwhile;
+							else: ?>
+                                <p>Nothing yet to be displayed!</p>
+							<?php endif; ?>
+                        </div>
+                    </div>
+                </section>
 			</main>
 		</div>
 	</div>
