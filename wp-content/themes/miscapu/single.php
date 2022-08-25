f6d60b639eaafdb5ff9f521d269740399ce00b05
miscapu <mignan2010@outlook.com>
2022-08-24 22:16:35 -0300

    Single.php: single.php file store blog content page in specific. In this class, let's call to our header and footer and loop for view a specific post of our theme miscapu

---
+++ wp-content/themes/miscapu/single.php
@@ -0,0 +1,62 @@
+<?php
+/***
+ * ===================
+ *  Call header
+ * ==================
+ */
+get_header();
+
+/**
+ * ===================
+ *  Loop of posts
+ * ==================
+ */
+?>
+
+<div id="primary">
+	<div id="main">
+		<div class="container">
+			<!--
+			==========================================================
+			Note important: Here don't we need check if exists posts
+			==========================================================
+			-->
+			<?php
+				while ( have_posts() ):
+					the_post();
+					?>
+
+					<!--
+					=========================
+					Article HTML
+					=========================
+					-->
+					<article id="post-<?php the_ID();?>" <?php post_class();?>>
+						<header>
+							<h1><?php the_title();?></h1>
+							<div class="meta-info">
+								<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+								<p>Categories: <?php the_category( ' ' );?></p>
+								<p>Tags: <?php the_tags( '', ', ' );?></p>
+							</div>
+						</header>
+						<div class="content">
+							<?php the_excerpt(); ?>
+						</div>
+					</article>
+
+				<?php
+				endwhile;
+			?>
+
+		</div>
+	</div>
+</div>
+
+<?php
+/**
+ * ==================
+ *  Get footer
+ * ==================
+ */
+get_footer();
